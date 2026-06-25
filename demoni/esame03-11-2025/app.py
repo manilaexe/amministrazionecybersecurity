a66808e7ade9b77c520c32d2a015f6de3ebc51d9
Manila <78729862+manilaexe@users.noreply.github.com>
2026-06-25 10:36:09 +0200

    Update app.py

--- demoni/esame03-11-2025/app.py
+++ demoni/esame03-11-2025/app.py
@@ -28,59 +28,60 @@ def walk(basepath, threshold, log_file_path): #basebath = directory da analizzar
         elif os.path.isdir(path): #chiede se e' una directory
             walk(path, threshold, log_file_path) #chiamata ricorsiva
 
-
 def main():
-    parser = argparse.ArgumentParser(description="old file detector")
+    parser = argparse.ArgumentParser(description="old file detector") #crea parser per leggere target, days, interval ,log
+
+    #gli argomenti da analizzare
     parser.add_argument(
         "--target", type=str, required=True, help="directory to scan for old files."
     )
     parser.add_argument(
-        "--days", type=int, required=True, help="number of days to consider a file old."
+        "--days", type=str, required=True, help="number to consider a file old."
     )
     parser.add_argument(
-        "--interval",
-        type=int,
-        required=True,
-        help="interval in seconds between checks.",
+        "--interval", type=int, required=True, help="interval in seconds between checks."
     )
     parser.add_argument(
-        "--log",
-        type=str,
-        required=True,
-        help="directory to save the log file.",
+        "--log", type=str, required=True, help="directory to save the log file."
     )
-    args = parser.parse_args()
+    args=parser.parse_args() #legge effettivamente i valori da terminale
 
+    #VALIDAZIONE DELL'INPUT
+    #controlli sul target
     if not os.path.isabs(args.target):
-        print("error: --target must be an absolute path.", file=sys.stderr)
+        print("error --target must be an absolute path.", file=sys.stderr)
         sys.exit(1)
     if not os.path.exists(args.target):
-        print("error: --target path does not exist.", file=sys.stderr)
+        print("error: --target path doesn't exists.", file=sys.stderr)
         sys.exit(1)
     if not os.path.isdir(args.target):
-        print("error: --target path is not a directory.", file=sys.stderr)
+        print("error: --target path must be a directory", file=sys.stderr)
         sys.exit(1)
 
-    if args.days <= 0:
-        print("error: --days must be a positive integer.", file=sys.stderr)
+    #controlli sui numeri
+    if args.days<=0:
+        print("error: --days must be a positive integer", file=sys.stderr)
         sys.exit(1)
-
-    if args.interval <= 0:
-        print("error: --interval must be a positive integer.", file=sys.stderr)
+    if args.interval<=0:
+        print("error: --interval must be a positive integer", file=sys.stderr)
         sys.exit(1)
-
+    
+    #validazione del log
     if not os.path.exists(args.log):
-        print("error: --log path does not exist.", file=sys.stderr)
+        print("error: --target path doesn't exists.", file=sys.stderr)
         sys.exit(1)
-    if not os.path.isdir(args.log):
-        print("error: --log path is not a directory.", file=sys.stderr)
+    if not os.path.isdir(args.target):
+        print("error: --target path must be a directory", file=sys.stderr)
         sys.exit(1)
-
-    log_file_path = os.path.join(args.log, "old-file-detector.log")
+ 
+    #creazione del file di log
+    log_file_path=os.path.join(args.log, "old-file-detector.log")
+    #loop infinito -> demone
     while True:
-        threshold = time.time() - (args.days * 86400)
-        walk(args.target, threshold, log_file_path)
-        time.sleep(args.interval)
+        threshold = time.time() - (args.days * 86400) #calcola la soglia
+        walk(args.target, threshold, log_file_path) #scansione 
+        time.sleep(args.interval) #aspetta n secondi prima di rifare tutto
+
 
 
 if __name__ == "__main__":
